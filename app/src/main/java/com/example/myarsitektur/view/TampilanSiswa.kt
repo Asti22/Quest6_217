3681bb09b17b84b3463c423f0cb4b06132bda5f2
Asti22 <uun3461@gmail.com>
2025-11-20 14:02:55 +0700

    Menyiapkan daftar pasangan label dan nilai untuk menampilkan data Siswa pada UI berdasarkan state terbaru

--- app/src/main/java/com/example/myarsitektur/view/TampilanSiswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/TampilanSiswa.kt
@@ -7,4 +7,10 @@ fun TampilSiswa(
 
     statusUISiswa: Siswa,
     onBackButtonClicked: () -> Unit
-){}
+){
+    val items = listOf(
+        Pair(stringResource(R.string.nama_lengkap), statusUISiswa.nama),
+        Pair(stringResource(R.string.jenis_kelamin), statusUISiswa.gender),
+        Pair(stringResource(R.string.alamat), statusUISiswa.alamat)
+    )
+}
